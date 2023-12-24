bfe1dffe58d978c7bc44716180152cb3890c308d
Ibrahim Goumrane <goumrane.ibrahim@ensam-casa.ma>
2023-12-24 17:05:17 +0100

    this modification add the fact that you can suggegest friend to another friend based on something like prim algo

--- classes_project.py
+++ classes_project.py
@@ -1,3 +1,6 @@
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
 preferences = (('Sports', 1),
                ('Gaming', 2),
                ('Fitness', 3),
@@ -9,14 +12,19 @@ preferences = (('Sports', 1),
                ('Parenting', 9),
                ('Fashion', 10),
                ('Other', 11))
-list_IAGI=[]      
+list_IAGI=[ ('ismail   moufatih', 1),
+            ('oussama  darrazi', 2),
+            ('mohsine  mohsine', 3),
+            ('yassin  ahmed', 4) ,
+            ('ibrahim  goumrane', 5)]     
 class Person:
     code_counter = 1  # Utilisation d'une variable de classe pour gérer le code de manière globale
-    def __init__(self, nom, pref=None, amis=None, code=None):
+    def __init__(self, nom, pref=None, amis=None, code=None ,black_list=[]):
         self.nom = nom
         self.code = Person.generate_code(code)
         self.pref = pref if pref is not None else []
         self.amis = amis if amis is not None else []
+        self.black_list=black_list
         self.add_person_list()
     @classmethod
     def generate_code(cls, provided_code):
@@ -27,9 +35,7 @@ class Person:
             cls.code_counter += 1
             return generated_code
     def add_person_list(self)  :
-        list_IAGI.append((self.nom,self.code))  
-    def get_code(self):
-        return self.code      
+        list_IAGI.append((self.nom,self.code) if (self.nom,self.code) not in list_IAGI else None)      
 class graphe :
     n=0
     def __init__(self,taille):
@@ -56,7 +62,9 @@ class graphe :
     def add_ele(self,p1,exsist=True): 
         self.increment_num_matrice(p1) if not exsist else None
         self.set_amis(p1,exsist) 
-        self.set_person_preference(p1,exsist)     
+        self.set_person_preference(p1,exsist)  
+    def get_friend(self,p1):
+        return [list_IAGI[p1.code-1][i] for i in range(self.n) if self.matrice_amis[p1.code-1][i]!=0]        
     def get_mean(self) :
         mean=0
         for i in self.rel_person_preference :
@@ -68,9 +76,46 @@ class graphe :
             'mat_amis':self.matrice_amis,
             'mat_pref':self.rel_person_preference,
         }
+def affichage_amis(p1,matrix,title):
+    #ajout de titre
+    plt.title(title)
+    list_amis=np.array(matrix['mat_amis'][p1.code-1])
+    
+    G = nx.Graph()
+
+    # Add edges from the adjacency matrix
+    for j in range(len(list_amis)):
+        if list_amis[j] != 0 and  p1.code-1 is not j:
+            G.add_edge(list_IAGI[p1.code-1][0], list_IAGI[j][0], weight=list_amis[j])  # Use weights from the adjacency matrix
+
+    # Visualize the graph
+    pos = nx.spring_layout(G)
+    nx.draw(G, pos, with_labels=True, node_size=1500, node_color='skyblue', font_size=7, font_color='black', font_weight='bold', edge_color='red', width=2.0, alpha=0.7)
+
+    #visualize the edges values
+    edgelabel=nx.get_edge_attributes(G,'weight')
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edgelabel)
+
+    
+    plt.show()    
+    
+def generate_deg_amis(matrice,code_src,code_inter,code_new) :
+    return (matrice[code_src][code_inter]+matrice[code_inter][code_new])/2
+def suggestin_friend_based_current_friend(p1,graphe):
+    current_friend_list=[i for i in range(graphe.n) if graphe.get_matrix()['mat_amis'][p1.code-1][i]!=0 and i!= p1.code-1]
+    for i in current_friend_list :
+        current_friend_list_i=[j for j in range(graphe.n) if graphe.get_matrix()['mat_amis'][i][j]!=0 and i!=j]
+        for j in current_friend_list_i :
+            if j+1 not in p1.black_list and j not in current_friend_list and j+1!=p1.code:
+                response=int(input(f'{list_IAGI[j][0]} this is a friend of {list_IAGI[i][0]} : do you want him to become your friend (1 or 0)'))
+                if response==1 :
+                    graphe.matrice_amis[p1.code-1][j]=generate_deg_amis(graphe.get_matrix()['mat_amis'],p1.code-1,i,j)
+                    print(f'{list_IAGI[j+1][0]} ajouter a la liste des amis')                              
+    title=f"Graph des nouveaux amis de {p1.nom}"                
+    affichage_amis(p1,graphe.get_matrix(),title)      
+  
     
 # test data des personnes 
-
 # deuxieme list correspond au activite et de degre de realisation de cette activites
 
 
--- mainfile.py
+++ mainfile.py
@@ -1,26 +1,51 @@
 from classes_project import *  
-p1=Person('ibrahim goumrane',
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+p1=Person('ibrahim\n goumrane',
           [[1,2],[3,5],[7,8],[5,2]]
-          ,[1,4,2,5],5) # code 5 nouveau person
-p2=Person('ismail moufatih',
+          ,[1,4,2,5],5)     # code 5 nouveau person
+p2=Person('ismail \nmoufatih',
           [[5,7],[2,4],[1,8]],
-          [1]) #code 1 valeur par defaut
-p3=Person('oussama darrazi',
+          )                 #code 1 valeur par defaut
+p3=Person('oussama \ndarrazi',
           [[8,5]],
-          [3]) #code 2 valeur par defaut
-p4=Person('mohsine mohsine',
+          )                 #code 2 valeur par defaut
+p4=Person('mohsine \nmohsine',
           [[8,5]],
-          [4]) #code 3 valeur par defaut
-p5=Person('yassin ahmed',
+          )                 #code 3 valeur par defaut
+p5=Person('yassin\n ahmed',
           [[8,4]],
-          [1]) #code 3 valeur par defaut
+          ) #code 3 valeur par defaut
 taille_IAGI=4 # nombre d'etudiant 4
 relation_p_class=graphe(taille_IAGI)
 
 relation_p_class.add_ele(p1,False)    #nombre d'etudiant devient 5 parceque il n'est pas exsistant au prealable
 relation_p_class.add_ele(p2,True)
-relation_p_class.add_ele(p3,True) 
+relation_p_class.add_ele(p3,True)
 
-matrix=relation_p_class.get_matrix()
-print(matrix['mat_amis'])
-print(list_IAGI)    
+matrix=relation_p_class.get_matrix() 
+
+def affichage_amis(p1):
+    #ajout de titre
+    plt.title(f"Graph des amis de {p1.nom}")
+    list_amis=np.array(matrix['mat_amis'][p1.code-1])
+    
+    G = nx.Graph()
+
+    # Add edges from the adjacency matrix
+    for j in range(len(list_amis)):
+        if list_amis[j] != 0 and  p1.code-1 is not j:
+            G.add_edge(list_IAGI[p1.code-1][0], list_IAGI[j][0], weight=list_amis[j])  # Use weights from the adjacency matrix
+
+    # Visualize the graph
+    pos = nx.spring_layout(G)
+    nx.draw(G, pos, with_labels=True, node_size=1500, node_color='skyblue', font_size=7, font_color='black', font_weight='bold', edge_color='red', width=2.0, alpha=0.7)
+
+    #visualize the edges values
+    edgelabel=nx.get_edge_attributes(G,'weight')
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edgelabel)
+
+    
+    plt.show()
+affichage_amis(p2)    
