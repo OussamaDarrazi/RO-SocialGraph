0c6a77bef1012354e2656bc1b0b2ae11eb90c1dc
Oussama Darrazi <34890717+OussamaDarrazi@users.noreply.github.com>
2023-12-30 23:05:12 +0100

    staticified suggestion delegate method

--- socialmedia/FriendSuggestionDelegate.py
+++ socialmedia/FriendSuggestionDelegate.py
@@ -2,6 +2,8 @@ from socialmedia.User import User
 
 
 class FriendSuggestionDelegate:
+    #pas de besoin d'instance
+    @staticmethod
     def suggestFriend(user: User):
         '''
         use the friend suggestion queue to pop friend suggestion
