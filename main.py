b729960e1ae68d74d908a34cfec4885550273e90
Oussama Darrazi <nostra.darrazi2002@gmail.com>
2024-01-06 22:23:24 +0100

    main func

--- main.py
+++ main.py
@@ -2,36 +2,37 @@ from socialmedia import User as usr
 from socialmedia.FriendSuggestionDelegate import FriendSuggestionDelegate
 from socialmedia.SocialGraph import SocialGraph
 from socialmedia.Interests import Interests
-from socialmedia.UserFactory import UserFactory
+# from socialmedia.UserFactory import UserFactory
 #defining users in 
 
-# usr.User(1,"Darrazi Oussama", {Interests.Music, Interests.Science_and_education, Interests.Culinary_arts})
-# usr.User(2,"Goumrane Ibrahim", {Interests.Gaming, Interests.Science_and_education})
-# usr.User(3,"Moufatih Ismail", {Interests.Gaming, Interests.Music, Interests.Photography, Interests.Fitness})
-# usr.User(4,"Name lastname1", {Interests.Lifestyle_and_travel})
-# usr.User(5,"Name lastname2", {Interests.Culinary_arts})
-# usr.User(6,"Name lastname3", {Interests.Music})
-# usr.User(7,"Name lastname4", {Interests.Parenting})
-# usr.User(8,"Name lastname5", {Interests.Other})
-
-UserFactory.usersFromDB()
-
-#testing userbase
-for _ in usr.User.Users:
-    print(_.id, _.username) 
+usr.User(0,"Darrazi Oussama", {Interests.Music, Interests.Science_and_education, Interests.Culinary_arts})
+usr.User(1,"Goumrane Ibrahim", {Interests.Gaming, Interests.Science_and_education})
+usr.User(2,"Moufatih Ismail", {Interests.Gaming, Interests.Music, Interests.Photography, Interests.Fitness})
+usr.User(3,"Miahi Othmane", {Interests.Lifestyle_and_travel})
+usr.User(4,"Tahiri Meryem", {Interests.Culinary_arts})
+usr.User(5,"Loro Triomphe", {Interests.Music})
+usr.User(6,"Chaffaa Mohammed", {Interests.Parenting})
+usr.User(7,"Dahouz Fatima", {Interests.Other})
 
 
-
-#testing commun interests
-
-#testing friendship functions
+graphe = SocialGraph(8)
 
 
-#TODO: add more tests
+#creation des amitiés
 
+usr.User.Users[0].addFriend(usr.User.Users[3], graphe)
+usr.User.Users[0].addFriend(usr.User.Users[5], graphe)
+usr.User.Users[0].addFriend(usr.User.Users[1], graphe)
 
+usr.User.Users[2].addFriend(usr.User.Users[4], graphe)
+usr.User.Users[2].addFriend(usr.User.Users[1], graphe)
+usr.User.Users[2].addFriend(usr.User.Users[5], graphe)
+usr.User.Users[2].addFriend(usr.User.Users[7], graphe)
 
+usr.User.Users[5].addFriend(usr.User.Users[3], graphe)
+usr.User.Users[5].addFriend(usr.User.Users[6], graphe)
 
+usr.User.Users[7].addFriend(usr.User.Users[3], graphe)
+usr.User.Users[7].addFriend(usr.User.Users[6], graphe)
+usr.User.Users[7].addFriend(usr.User.Users[5], graphe)
 
-######################## MAIN APP #####################
-#TODO: add main functionality
